3b1585af4d220947a3e55fa53bd470b7201b5f2b
emrecoskun705 <emrecoskun705@gmail.com>
2023-09-04 17:16:57 +0300

    Change exception middleware because I use Serilog for exception catching middleware I only send InternalServer status code in our custom exception middleware

--- Unitagram.WebAPI/Controllers/v1/AccountController.cs
+++ Unitagram.WebAPI/Controllers/v1/AccountController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Connections;
 using Microsoft.AspNetCore.Mvc;
 using Unitagram.Application.Contracts.Identity;
 using Unitagram.Application.Models.Identity.Authentication;
@@ -8,7 +9,7 @@ using Unitagram.Application.Models.Identity.Register;
 namespace Unitagram.WebAPI.Controllers.v1;
 
 /// <summary>
-/// 
+/// Controller responsible for user account-related actions.
 /// </summary>
 [AllowAnonymous]
 [ApiVersion("1.0")]
@@ -17,19 +18,19 @@ public class AccountController : CustomControllerBase
     private readonly IAuthService _authService;
 
     /// <summary>
-    /// 
+    /// Initializes a new instance of the <see cref="AccountController"/> class.
     /// </summary>
-    /// <param name="authService"></param>
+    /// <param name="authService">The authentication service to handle user account operations.</param>
     public AccountController(IAuthService authService)
     {
         _authService = authService;
     }
 
     /// <summary>
-    /// Login Test
+    /// Processes a login request, authenticating the user and returning an authentication response.
     /// </summary>
-    /// <param name="request"></param>
-    /// <returns></returns>
+    /// <param name="request">The authentication request containing user credentials.</param>
+    /// <returns>An <see cref="ActionResult{T}"/> containing an <see cref="AuthResponse"/>.</returns>
     [HttpPost("login")]
     public async Task<ActionResult<AuthResponse>> Login(AuthRequest request)
     {
@@ -38,10 +39,10 @@ public class AccountController : CustomControllerBase
     }
     
     /// <summary>
-    /// Register Request
+    /// Processes a registration request, creating a new user account and returning a registration response.
     /// </summary>
-    /// <param name="request"></param>
-    /// <returns></returns>
+    /// <param name="request">The registration request containing user information.</param>
+    /// <returns>An <see cref="ActionResult{T}"/> containing a <see cref="RegisterResponse"/>.</returns>
     [HttpPost("register")]
     public async Task<ActionResult<RegisterResponse>> Register(RegisterRequest request)
     {
@@ -50,13 +51,14 @@ public class AccountController : CustomControllerBase
     }
     
     /// <summary>
-    /// Refresh Token test
+    /// Refreshes an authentication token using a refresh token and returns an updated authentication response.
     /// </summary>
-    /// <param name="request"></param>
-    /// <returns></returns>
+    /// <param name="request">The refresh token request containing a refresh token.</param>
+    /// <returns>An <see cref="ActionResult{T}"/> containing an <see cref="AuthResponse"/>.</returns>
     [HttpPost("refresh-token")]
     public async Task<ActionResult<AuthResponse>> RefreshToken(RefreshRequest request)
     {
+        throw new ConnectionAbortedException("aborted connection");
         var result = await _authService.RefreshToken(request);
         return result.ToOk(HttpContext);
     }
--- Unitagram.WebAPI/Middleware/ExceptionMiddleware.cs
+++ Unitagram.WebAPI/Middleware/ExceptionMiddleware.cs
@@ -1,50 +1,35 @@
-using System.Net;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using Unitagram.Application.Exceptions;
-using Unitagram.WebAPI.Models;
-
 namespace Unitagram.WebAPI.Middleware;
 
+/// <summary>
+/// 
+/// </summary>
 public class ExceptionMiddleware
 {
     private readonly RequestDelegate _next;
-    private readonly ILogger<ExceptionMiddleware> _logger;
 
-    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="next"></param>
+    public ExceptionMiddleware(RequestDelegate next)
     {
         _next = next;
-        _logger = logger;
     }
-
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="httpContext"></param>
     public async Task InvokeAsync(HttpContext httpContext)
     {
         try
         {
             await _next(httpContext);
         }
-        catch (Exception ex)
+        catch (Exception)
         {
-            await HandleExceptionAsync(httpContext, ex);
+            httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
         }
     }
-
-    private async Task HandleExceptionAsync(HttpContext httpContext, Exception ex)
-    {
-        HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
-        ProblemDetails problem = new();
-        problem = new ProblemDetails()
-        {
-            Title = ex.Message,
-            Status = (int)statusCode,
-            Type = nameof(HttpStatusCode.InternalServerError),
-            Detail = ex.StackTrace,
-        };
-
-
-        httpContext.Response.StatusCode = (int)statusCode;
-        var logMessage = JsonConvert.SerializeObject(problem);
-        _logger.LogError(logMessage);
-        await httpContext.Response.WriteAsJsonAsync(problem);
-    }
+    
 }
