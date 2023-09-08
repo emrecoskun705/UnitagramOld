cdfc5c841a025777c0a63e9e1a835b90db7f1369
emrecoskun705 <emrecoskun705@gmail.com>
2023-09-08 23:01:59 +0300

    add base entity

--- Unitagram.Domain/UniversityUser.cs
+++ Unitagram.Domain/UniversityUser.cs
@@ -1,6 +1,8 @@
+using Unitagram.Domain.Common;
+
 namespace Unitagram.Domain;
 
-public class UniversityUser
+public class UniversityUser : BaseEntity
 {
     public Guid UserId { get; set; }
     public Guid UniversityId { get; set; }
