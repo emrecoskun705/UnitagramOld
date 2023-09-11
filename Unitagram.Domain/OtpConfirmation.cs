f14566ee076076f791dee84ff5be8168b8b5fcad
emrecoskun705 <emrecoskun705@gmail.com>
2023-09-11 21:33:20 +0300

    add otp confirmation

--- Unitagram.Domain/OtpConfirmation.cs
+++ Unitagram.Domain/OtpConfirmation.cs
@@ -1,10 +1,12 @@
+using Unitagram.Domain.Common;
+
 namespace Unitagram.Domain;
 
-public class OtpConfirmation
+public class OtpConfirmation : BaseEntity
 {
     public Guid UserId { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Value { get; set; } = string.Empty;
     public int RetryCount { get; set; }
-    public DateTime? RetryDateTime { get; set; }
+    public DateTimeOffset? RetryDateTimeUtc { get; set; }
 }
